805662a2e44b20efe02e7c1edcc4836cc0fe734e
Yatimisi <yatimisi2012@gmail.com>
2019-01-02 20:22:22 +0800

    update

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
 from utils.forms import DeleteConfirmForm
@@ -6,12 +7,12 @@ from utils.forms import DeleteConfirmForm
 from .forms import BookForm
 from .models import Book
 
-
+@login_required
 def index(request):
     books = Book.objects.all()
     return render(request, 'books/index.html', {'books': books})
 
-
+@login_required
 def show(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'books/show.html', {'book': book})
--- core/settings.py
+++ core/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+LOGIN_URL = '/login/'
+
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/login/'
