84691d2f1e8cf128e2e4d9efed74559fb699567c
Yatimisi <yatimisi2012@gmail.com>
2019-01-07 19:57:56 +0800

    update

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, 
+    PasswordResetView, PasswordResetConfirmView,
+)
 from django.shortcuts import redirect
 from django.urls import path, include
 
@@ -28,6 +31,8 @@ login_params = {
 
 password_reset_params = {
     'template_name': 'users/password_reset.html',
+    'email_template_name': 'users/password_reset/email.html',
+    'subject_template_name': 'users/password_reset/subject.txt',
 }
 
 urlpatterns = [
@@ -35,6 +40,13 @@ urlpatterns = [
     path('login/', LoginView.as_view(**login_params), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
+    path('password-reset', 
+         PasswordResetView.as_view(**password_reset_params), 
+         name='password_reset'),
+    path('password-set/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(),
+         name='password_set'),
+    
     path('books/', include('books.urls')),
     path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
